579817ae3226bad1bf9a5de7bf4686df4e534922
Code Nation <codenation@CN-3.lan>
2019-03-05 14:10:43 +0000

    table

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "./Content.css";
+import Table from './Table'
 
 class Content extends Component {
   state = {};
   render() {
-    return <div className="content">Content</div>;
+    return <div className="content">Content;
+    <Table />
+    </div>
   }
 }
 
---
+++ src/Components/Content/Table.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+
+
+class Table extends Component {
+
+    state = {
+        columnDefs: [
+            { headerName: "Title", field: "title" },
+            { headerName: "Artist", field: "artist" },
+            { headerName: "Album", field: "album" },
+            { headerName: "Released", field: "released" },
+            { headerName: "Duration", field: "duration" }
+
+
+
+        ],
+        rowData: [
+            { title: "Wow", artist: "Post Malone", album: "", released: "2018-12-24", duration: "2:30" },
+            { title: "Wow", artist: "Post Malone", album: "", released: "2018-12-24", duration: "2:30" },
+            { title: "Wow", artist: "Post Malone", album: "", released: "2018-12-24", duration: "2:30" },
+            { title: "Wow", artist: "Post Malone", album: "", released: "2018-12-24", duration: "2:30" },
+            { title: "Wow", artist: "Post Malone", album: "", released: "2018-12-24", duration: "2:30" },
+            { title: "Wow", artist: "Post Malone", album: "", released: "2018-12-24", duration: "2:30" },
+            { title: "Wow", artist: "Post Malone", album: "", released: "2018-12-24", duration: "2:30" },
+            { title: "Wow", artist: "Post Malone", album: "", released: "2018-12-24", duration: "2:30" },
+
+           
+            
+        ]
+    }
+
+render() {
+    return (
+        <div
+            className="ag-theme-balham"
+            style={{ height: '800px', width: '900px'}}
+        >
+            <AgGridReact
+                columnDefs={this.state.columnDefs}
+                rowData={this.state.rowData}>
+            </AgGridReact>
+        </div>
+    );
+}
+
+}
+
+export default Table;
+
+
