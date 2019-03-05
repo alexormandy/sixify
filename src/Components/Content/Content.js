941dddeac8cf773bf972845d1ba8621b62855e6f
Administrator <administrator@CN-2.lan>
2019-03-05 16:35:08 +0000

    pre-merged georgi

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import "./Content.css";
+import Search from "./Search"
 
 class Content extends Component {
   state = {};
   render() {
-    return <div className="content">Content</div>;
+    return(
+    
+    
+    <div className="Search"> 
+    <Search /></div>) 
+    
   }
 }
 
