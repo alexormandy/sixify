c58eb110149b0fa04e9daa7ff94ddbb2c8a5acf6
KieranLProctor <kieran.lee.proctor@gmail.com>
2019-03-05 09:30:26 +0000

    Started work on the player bar

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -2,9 +2,16 @@ import React, { Component } from "react";
 import "./Player.css";
 
 class Player extends Component {
+
   state = {};
+
   render() {
-    return <div className="player">Player</div>;
+
+    return(
+      <div className="player">Player
+        
+      </div>
+    );
   }
 }
 
