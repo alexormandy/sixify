35e668380b384f5fd2882c6fe35aed169c3a9981
Alexander Ormandy <alexanderormandy@Alexanders-MBP.lan>
2019-03-05 11:37:05 +0000

    sidebar changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,22 @@ import "./Navbar.css";
 class Navbar extends Component {
   state = {};
   render() {
-    return <div className="navbar">Navbar</div>;
+    return (
+      <div className="navbar">
+        <button> Home </button>
+        <button> Browse </button>
+        <button> Radio </button>
+        <h3> Playlists </h3>
+
+        <ul> Playlist 1</ul>
+        <ul> Playlist 2</ul>
+        <ul> Playlist 3</ul>
+
+        <button> New Playlist </button>
+
+        <div className="currentlyplayingartwork"> </div>
+      </div>
+    );
   }
 }
 
