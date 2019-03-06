8ed8d5a6a9f12f3faa97ffb825681ed930139887
Alexander Ormandy <alexanderormandy@Alexanders-MBP.lan>
2019-03-06 15:49:40 +0000

    removing node modules

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,23 +1,20 @@
 import React, { Component } from "react";
 import "./Content.css";
 import Toolbar from "../Search/Search";
-import Search from "./Search";
+import Search from "../Search/Search";
 import Table from "./Table";
-
-
 
 class Content extends Component {
   state = {};
   render() {
-    return(
-    <div className="content">
-      <Search />
-      <Table />
-       <Toolbar />
-    </div>
-    ) 
-
+    return (
+      <div className="content">
+        <Search />
+        <Table />
+        <Toolbar />
+      </div>
+    );
   }
 }
 
-export default Content
+export default Content;
