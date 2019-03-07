a7b473f99e6d8610d7b1034553cd9db70ff4e934
Administrator <administrator@CN-2.lan>
2019-03-07 11:52:35 +0000

    changes

--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -11,11 +11,10 @@ const Toolbar = props => (
         <a className="toolbar_navigation-item" href="/">Premium</a>
         <a className="toolbar_navigation-item1" href="/">Help</a>
         <a className="toolbar_navigation-item2" href="/">Download</a>
-        </div>
-        <div>
         <a className="toolbar_navigation-item3" href="/">Sign up</a>
         <a className="toolbar_navigation-item4" href="/">Log In</a>
         </div>
+        
         <Search />
         <div>
         <a className="toolbar_navigation-item5" href="/">Playlist</a>
