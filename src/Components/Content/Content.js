da78411d6e9cc0df1bd3e3264de617c1d338347b
Code Nation 18 <codenation@CN-18.lan>
2019-03-08 11:37:43 +0000

    Grid overhaul

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import "./Content.css";
 import Toolbar from "../Search/Search";
 
-import Table from "./Table";
 
 class Content extends Component {
   state = {};
   render() {
     return (
       <div className="content">
-        <Toolbar />
-        <Table />
+        <div className="content-Flex-Container">
+          <div className="top-Content">
+            <div className="toolbar"><Toolbar /></div>
+          </div>
+          <div className="bottom-Content">
+            <div className="playlist-Table"></div> 
+          </div>
+        </div>
       </div>
     );
   }
